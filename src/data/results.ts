39e4fd2b64ff3b36eb3f958ffba5199df81e9e11
aditya_demas <adityademas@gmail.com>
2024-10-19 19:49:04 +0700

    up

--- src/data/results.ts
+++ src/data/results.ts
@@ -5,36 +5,36 @@ export const results: Result[] = [
     type: 'existing',
     title: 'Petambak Eksis',
     description: 'Kamu adalah petambak yang selalu up-to-date dengan tren terbaru dalam budidaya udang.',
-    imageUrl: 'https://example.com/petambak-eksis.png'
+    imageUrl: '/image/petambak_eksis.png'
   },
   {
     type: 'fanatic',
     title: 'Pakar Udang',
     description: 'Kamu memiliki pengetahuan mendalam tentang budidaya udang dan selalu mencari cara untuk meningkatkan hasil panen.',
-    imageUrl: 'https://example.com/pakar-udang.png'
+    imageUrl: '/image/pakar_udang.png'
   },
   {
     type: 'shrimp',
     title: 'Juragan Tambak',
     description: 'Kamu adalah pemilik tambak yang sukses dan memiliki pengalaman luas dalam industri udang.',
-    imageUrl: 'https://example.com/juragan-tambak.png'
+    imageUrl: '/image/juragan_petambak.png'
   },
   {
     type: 'ordinary',
     title: 'Petambak Biasa',
     description: 'Kamu menjalankan tambak udang dengan cara tradisional dan mengandalkan pengalaman yang sudah teruji.',
-    imageUrl: 'https://example.com/petambak-biasa.png'
+    imageUrl: '/image/petambak_biasa.png'
   },
   {
     type: 'trader',
     title: 'Juragan Tambak',
     description: 'Bagimu, juragan tambak, bisnis udang adalah salah satu dari banyaknya opsi bisnis yang menarik. Menimbang kemungkinan profit dan loss sudah menjadi kegiatan utama dalam berbisnis.',
-    imageUrl: 'https://example.com/juragan-tambak.png'
+    imageUrl: '/image/juragan_petambak.png'
   },
   {
     type: 'hi-tech',
     title: 'Kyaknya Km Udang',
     description: 'Kamu menggunakan teknologi terbaru untuk mengoptimalkan produksi udang di tambakmu.',
-    imageUrl: 'https://example.com/kyaknya-km-udang.png'
+    imageUrl: '/image/kyaknya_km_udang.png'
   }
 ];
